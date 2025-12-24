96c97b1d521017bb13208c1bfce515ff4d041aed
gowthami <gowt40632@gmail.com>
2025-12-24 15:38:00 +0530

    Express Created

---
+++ Backend/config/db.js
@@ -0,0 +1,12 @@
+const mongoose= require("mongoose");
+const connectDB=async()=>{
+    try{
+     await mongoose.connect(process.env.MONGO_URL);
+     console.log("mongoDB connected successfully🎸🕊️🦋")
+    }catch(err){
+        console.log(err)
+  
+    }
+
+}
+module.exports=connectDB;
